65c701a7841fef44833c39292b833cc0549cd5b7
Caio Cabral <caxcabral@protonmail.ch>
2019-09-28 11:29:34 -0300

    Chi-squared

--- Metricas/MetricasTCC.py
+++ Metricas/MetricasTCC.py
@@ -11,18 +11,18 @@ class raMetricas:
     def intersect(bd, itemset):
         # Cria um vetor de colunas (matriz) de bd, usando a lista itemset como referencia de indice.
         base = bd[:, np.array(itemset) - 1]
-        # Funcao all do np achata a matriz usando and logico no eixo passado como parametro.
+        # Metodo all do np achata a matriz usando and logico no eixo passado como parametro.
         return base.all(axis=1)
 
     @staticmethod
-    def abSupp(bd, itemset, itemset2=None, negativo=False):
-        if itemset2 is not None:
-            itemset.append(itemset2)
+    def abSupp(bd, itemset1, itemset2=None, negativo=False):
+        itemset = itemset1 + (itemset2 if itemset2 is not None else [])
         # Suporte negativo verifica a existência de transações que nao contem nenhum item do itemset
         # Chama a msma funcao de intersecao, mas com a base de dados invertida.
         if negativo:
-            return np.sum(raMetricas.intersect(itemset, abs(bd-1)), axis=0)
-        return np.sum(raMetricas.intersect(itemset, bd), axis=0)
+            return np.sum(raMetricas.intersect(abs(bd - 1), itemset), axis=0)
+        # np.sum achata o vetor usando soma no eixo passado como parametro.
+        return np.sum(raMetricas.intersect(bd, itemset), axis=0)
 
     @staticmethod
     def relSupp(bd, itemset, itemset2=None, negativo=False):
@@ -37,7 +37,9 @@ class raMetricas:
 
     @staticmethod
     def conf(bd, antc, consq):
-        return raMetricas.abSupp(bd, antc, consq) / raMetricas.abSupp(bd, antc)
+        raSup = raMetricas.abSupp(bd, antc, consq)
+        antSup = raMetricas.abSupp(bd, antc)
+        return raSup / antSup
 
     @staticmethod
     def addedValue(bd, antc, consq):
@@ -65,6 +67,32 @@ class raMetricas:
         cf = raMetricas.conf(bd, ant, cons) - raMetricas.relSupp(bd, cons)
         return cf / raMetricas.relSupp(bd, cons, negativo=True)
 
+    @staticmethod
+    def chiSqrd(bd, antc, cons):
+        #    l1 = [5,15,5]
+        #    l2 = [50,10,15]
+        #    l3 = [55,25,20]
+        #    a= np.matrix([l1, l2, l3])
+
+        bdInv = abs(bd - 1)
+
+        # Montando tabela de contingencia
+        lin = [raMetricas.intersect(bd, antc), raMetricas.intersect(bdInv, antc)]
+        col = [raMetricas.intersect(bd, cons), raMetricas.intersect(bdInv, cons)]
+
+        tbCtg = np.matrix([[np.sum(i * j) for i in lin] for j in col])
+
+        df = tbCtg.shape[1] - 1
+
+        calcEsp = lambda i, j: np.sum(tbCtg[i, :]) * np.sum(tbCtg[:, j]) / np.sum(tbCtg)
+
+        esperado = [calcEsp(i, j) for i in range(tbCtg.shape[0]) for j in range(tbCtg.shape[0])]
+        observado = tbCtg.getA1()
+
+        chi, p = chisquare(f_obs=observado, f_exp=esperado, ddof=df)
+        return chi
+
+
 mt = pd.read_table(baseDados, delim_whitespace=True, dtype="str", header=None)
 mtBinaria = pd.get_dummies(mt)
 dados = mtBinaria.astype('int').to_numpy()
@@ -78,5 +106,4 @@ regras["cons"] = regras["cons"].str.split()
 regras["antc"] = regras["antc"].str.split()
 '''
 
-print(allConf([1, 6]))
-print(allConf2([1, 6]))
+print(raMetricas.conf(dados, [1], [6, 18]))
