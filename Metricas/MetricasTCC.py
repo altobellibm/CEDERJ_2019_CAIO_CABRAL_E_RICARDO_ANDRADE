88ab57b4b3f7ff8ce8b07857f6318108e7b9d000
Caio Cabral <caxcabral@protonmail.ch>
2019-09-28 09:54:55 -0300

    Alteracao em allConf e criacao de subSup e casualConf

--- Metricas/MetricasTCC.py
+++ Metricas/MetricasTCC.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 from scipy.stats import chisquare
 
+baseDados = "BPressureNishiBook.dat"
+baseRegras = "BPressureNishiBook.txt"
+
 
 class raMetricas:
     @staticmethod
@@ -16,16 +19,21 @@ class raMetricas:
         if itemset2 is not None:
             itemset.append(itemset2)
         # Suporte negativo verifica a existência de transações que nao contem nenhum item do itemset
-        # Chamei a msma funcao de intersecao com a base de dados invertida.
+        # Chama a msma funcao de intersecao, mas com a base de dados invertida.
         if negativo:
             return np.sum(raMetricas.intersect(itemset, abs(bd-1)), axis=0)
         return np.sum(raMetricas.intersect(itemset, bd), axis=0)
 
     @staticmethod
     def relSupp(bd, itemset, itemset2=None, negativo=False):
-        if itemset2 is not None:
-            itemset.append(itemset2)
         return raMetricas.abSupp(bd, itemset, itemset2, negativo) / bd.shape[0]
+
+    @staticmethod
+    def subSupp(bd, itemset):
+        # Calcula o suporte de todos os 1-itemsets contidos em itemset.
+        subSup = bd[:, np.array(itemset) - 1]
+        subSup = np.sum(subSup, axis=0)
+        return subSup
 
     @staticmethod
     def conf(bd, antc, consq):
@@ -37,10 +45,35 @@ class raMetricas:
 
     @staticmethod
     def allConf(bd, itemset):
-        vetorSups = np.fromiter((raMetricas.relSupp(bd, i) for i in np.array(itemset)), itemset.dtype)
-        maxSup = np.max(vetorSups)
-        return raMetricas.relSupp(bd, itemset) / maxSup
+        max = raMetricas.subSupp(bd, itemset)
+        max = np.max(max)
+        max /= bd.shape[0]
+        return raMetricas.relSupp(bd, itemset) / max
 
     @staticmethod
     def casualSupp(bd, ant, cons):
         return raMetricas.relSupp(bd, ant, cons) + raMetricas.relSupp(bd, ant, cons, negativo=True)
+
+    @staticmethod
+    def casualConf(bd, ant, cons):
+        conf1 = raMetricas.conf(bd, ant, cons)
+        conf2 = raMetricas.abSupp(bd, ant, cons, negativo=True) / raMetricas.abSupp(ant, negativo=True)
+        return (conf1 + conf2) / 2
+
+
+
+mt = pd.read_table(baseDados, delim_whitespace=True, dtype="str", header=None)
+mtBinaria = pd.get_dummies(mt)
+dados = mtBinaria.astype('int').to_numpy()
+
+'''regras = pd.read_table(baseRegras, sep="#", names=("AR", "sup", "cnf"))
+regras["antc"], regras["cons"] =  regras["AR"].str.split("==>").str
+del regras["AR"]
+regras["sup"] = regras["sup"].str.replace("SUP:", "").astype('float')
+regras["cnf"] = regras["cnf"].str.replace("CONF:", "").astype('float')
+regras["cons"] = regras["cons"].str.split()
+regras["antc"] = regras["antc"].str.split()
+'''
+
+print(allConf([1, 6]))
+print(allConf2([1, 6]))
